27e30c0cdd699a7e8ae3cf938378a61b4122c1d9
Drina <Drinadomandiola@gmail.com>
2019-08-20 11:47:58 +1200

    first programming session game

---
+++ New Unity Project/Assets/_Scripts/BallBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    public float Ballthrust;
+    private Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        RB.AddForce(transform.forward * Ballthrust,ForceMode.Acceleration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/_Scripts/PaddleInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleInput : MonoBehaviour
+{
+    public float speed;
+    public float ClampValue;
+    public void MovePaddle(float amountToMove)
+    {
+        Vector3 ClampedGameobjectPosition=
+        gameObject.transform.position + new Vector3(0, 0,(amountToMove*speed));
+
+        ClampedGameobjectPosition.z = Mathf.Clamp(ClampedGameobjectPosition.z, -ClampValue, ClampValue);
+        gameObject.transform.position = ClampedGameobjectPosition;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        MovePaddle(Input.GetAxis("Horizontal"));
+       
+
+       gameObject.transform.position +=new Vector3(0,0,Input.GetAxis("Horizontal"));
+       
+    }
+
+      
+    }
+
+
+
