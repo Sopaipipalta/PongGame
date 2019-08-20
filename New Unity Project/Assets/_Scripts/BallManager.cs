bcd3c938416e6229268f3de9f34afcbe159dadf2
Drina <Drinadomandiola@gmail.com>
2019-08-21 11:36:40 +1200

    Class2 Ballbehaviour/paddleinput/goals

---
+++ New Unity Project/Assets/_Scripts/BallManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    public static BallManager instance;
+    private List<int> potentialAngles = new List<int>
+    {
+        60,160,270,20
+    };
+    
+
+
+    public GameObject ball;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        InstantiateBall();
+    }
+    public void InstantiateBall()
+    {
+        int AngleIndex = Random.Range(0, 4);
+        GameObject ballCopy = Instantiate(ball, Vector3.zero, Quaternion.Euler(0, potentialAngles[AngleIndex],0));
+
+        
+
+    }
+
+  
+    
+          
+}
+
